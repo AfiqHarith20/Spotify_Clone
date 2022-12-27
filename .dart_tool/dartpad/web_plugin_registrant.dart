22f040bb48d6eb67126790d532e3e607c53a79e7
Afiq Harith <afiqharith15@gmail.com>
2022-12-27 15:06:33 +0800

    update library

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,26 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:assets_audio_player_web/web/assets_audio_player_web.dart';
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_performance_web/firebase_performance_web.dart';
+import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AssetsAudioPlayerWebPlugin.registerWith(registrar);
+  FirebaseFirestoreWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebasePerformanceWeb.registerWith(registrar);
+  FirebaseStorageWeb.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
