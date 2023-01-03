4fe70da08360a8da662b439e0b42947b52c94afa
Afiq Harith <afiqharith15@gmail.com>
2023-01-03 17:20:29 +0800

    genre_page

---
+++ lib/screens/genre_page/genre_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:spotify_clone_provider/controllers/main_controller.dart';
+
+import 'package:flutter/material.dart';
+
+class GenrePage extends StatelessWidget {
+  final TagsModel tag;
+  final MainController con;
+  const GenrePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
