b143295ccab7124ea47b19c80958869194f8a461
Afiq Harith <68229091+AfiqHarith20@users.noreply.github.com>
2023-01-06 08:51:08 +0800

    Update genre_page.dart

--- lib/screens/genre_page/genre_page.dart
+++ lib/screens/genre_page/genre_page.dart
@@ -1,10 +1,4 @@
 
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:spotify_clone_provider/controllers/main_controller.dart';
-
-import 'package:flutter/material.dart';
-
 import 'dart:ui';
 
 import 'package:cached_network_image/cached_network_image.dart';
@@ -25,11 +19,6 @@ import 'package:spotify_clone_provider/utils/loading.dart';
 class GenrePage extends StatelessWidget {
   final TagsModel tag;
   final MainController con;
-  const GenrePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   const GenrePage({
     Key? key,
     required this.tag,
@@ -275,3 +264,4 @@ class GenrePage extends StatelessWidget {
     );
   }
 }
+
