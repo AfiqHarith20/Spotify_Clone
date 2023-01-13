0c35a3a3f46b59023ad7630cdca22abf90f8018a
Afiq Harith <68229091+AfiqHarith20@users.noreply.github.com>
2023-01-13 15:10:20 +0800

    Update 10 (#15)
    * playlist file
    
    * Playing controls, playlist, position file add
    
    * update

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/Mobile Application/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/Mobile Application/Spotify_clone_provider"
+export "FLUTTER_APPLICATION_PATH=/Users/zaid/Documents/GitHub/Spotify_Clone"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/zaid/Documents/GitHub/Spotify_Clone/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
+export "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/zaid/Documents/GitHub/Spotify_Clone/.dart_tool/package_config.json"
--- lib/screens/library/library.dart
+++ lib/screens/library/library.dart
@@ -1,5 +1,3 @@
-import 'dart:js';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/src/widgets/container.dart';
